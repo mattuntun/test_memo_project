ee41806d0e2dc1b3eb482daa07f8072c2c149bc9
mattuntun <mattuntun3@gmail.com>
2020-03-03 16:49:57 +0900

    モデル作成⇒DBからデータ取り出し、コントローラ作成、ビューにDB情報を取得して掲載

---
+++ test_memo/app/Http/Controllers/MemosController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Memo;
+
+class MemosController extends Controller
+{
+    public function index() {
+        $memos = Memo::orderBy('created_at','desc')->get();
+        return view('index',['memos'=>$memos]);
+    }
+
+
+    
+
+
+}
